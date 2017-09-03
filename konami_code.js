374d72cfb14f58421394911b027c0f4ad5729c27
Natasha Petrus <hello@natashapetrus.com>
2017-09-03 17:42:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const body = document.querySelector('body')
 
-body.addEventListener('keydown', function(e) {
-  return init(e);
-})
-
-let index = 0;
-
-function init(e) {
-  const key = parseInt(e.detail || e.which);
-
-  if (key === code[index]) {
-    index++;
-
-    if (index === code.length) {
-      alert("Congrats!");
+function init() {
+  const codeListener = document.body;
+  codeListener.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+    if(key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert('cool');
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
+  })
 }
